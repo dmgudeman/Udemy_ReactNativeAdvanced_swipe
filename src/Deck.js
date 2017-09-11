f01d24bb0768d65454956bebb5b69f1a416b148d
davidmgudeman <davidmgudeman@gmail.com>
2017-09-11 09:03:50 -0700

    swipes left and right with one conditional helper function

--- src/Deck.js
+++ src/Deck.js
@@ -9,6 +9,7 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 400;
 
 class Deck extends Component {
    
@@ -22,15 +23,23 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD){
-          console.log('swipe right');
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left');
+          this.forceSwipe('left');
         } else {
         this.resetPosition();
         }
       }
     });
     this.state = { panResponder, position };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH * 1.5 : - SCREEN_WIDTH * 1.5;
+    Animated.timing(this.state.position, { 
+      toValue: {x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start();
   }
 
   resetPosition() {
